2febb8c80850f8887166a0ca21c4f6cb89cbc7bb
setsin chutsanayothin <setsin2549@gmail.com>
2026-03-25 10:14:49 +0700

    2

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
-//import * as fs from 'fs'
-//import * as path from "path"
+//import * as fs from 'fs';
+//import * as path from 'path';
 
 @Injectable()
-export class AppService {
+export class ProductService {
   //private filePath = path.join(process(cwd),"data","product.json")
   getHello(): string {
     return 'Hello World!';
