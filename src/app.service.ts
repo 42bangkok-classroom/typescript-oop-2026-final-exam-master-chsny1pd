f6429dc9daa4b78f9e16eecac12976157c80c400
setsin chutsanayothin <setsin2549@gmail.com>
2026-03-25 10:48:40 +0700

    3

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ApiResponse } from './interfaces/response.interface';
 //import * as fs from 'fs'
 //import * as path from "path"
 
 @Injectable()
 export class AppService {
   //private filePath = path.join(process(cwd),"data","product.json")
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): ApiResponse<{
+    service: 'purchase-api';
+    version: '1.0.0';
+  }> {
+    return {
+      success: true,
+      data: {
+        service: 'purchase-api',
+        version: '1.0.0',
+      },
+      message: 'Hello NestJS',
+    };
   }
 }
